455312ebd04eaafff2e670b61743fb16c1882bea
silverknight59222 <silverknight59222@gmail.com>
2023-09-28 16:21:50 +0200

    Agenda Event modal responsive

--- src/components/pages/dashboard/Dashboard/Agenda/EventDetails.tsx
+++ src/components/pages/dashboard/Dashboard/Agenda/EventDetails.tsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import { CrossIcon } from "@/components/utilis/Icons";
 import { ColorsThemeA } from "@/components/utilis/Themes";
 import { ChatSendIcon } from "@/components/utilis/Icons";
+import './index.css';
+
+// Le reste de votre code JSX...
+
 
 interface Booking {
   id: string;
@@ -24,6 +28,7 @@ const AddServiceModal = (props: BookingDetail) => {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<{ content: string, sent: boolean }[]>([]);
 
+  {/* TODO Replace with the correction message function */ }
   const sendMessage = () => {
     if (message) {
       setMessages([...messages, { content: message, sent: true }]);
@@ -44,7 +49,7 @@ const AddServiceModal = (props: BookingDetail) => {
 
 
   return (
-    <div className="relative bg-white rounded-xl px-5 pb-5 shadow-lg" style={{ minWidth: '500px', minHeight: '600px' }}>
+    <div className="relative bg-white rounded-xl px-5 pb-5 shadow-lg mt-10 modal">
       {/* Icône de fermeture */}
       <div
         className={`absolute -right-3 -top-7 cursor-pointer my-3 rounded-lg ${ColorsThemeA.ohcVerticalGradient_A} shadow-md flex items-center justify-center`}
@@ -91,9 +96,9 @@ const AddServiceModal = (props: BookingDetail) => {
 
 
         {/* Zone de messagerie */}
-        <div className="flex flex-col gap-2 mt-4 ">
+        <div className="modalChat flex flex-col gap-2 mt-4  ">
           {/* Conversation */}
-          <div className="border border-gray-300 rounded-xl p-2 rounded-bl-lg overflow-auto h-40 bg-stone-100 shadow-inner mb-2 ">
+          <div className=" border border-gray-300 rounded-xl p-2 rounded-bl-lg overflow-auto h-40 bg-stone-100 shadow-inner mb-2 ">
             {messages.map((msg, index) => (
               <div key={index} className={`${msg.sent ? 'text-right' : 'text-left '} mb-2`}>
                 <div
@@ -106,16 +111,16 @@ const AddServiceModal = (props: BookingDetail) => {
           </div>
 
           {/* Zone d'écriture et envoi - ajouter nKeyDown={handleKeyPress} pour valider le message avec Enter */}
-          <div className="flex gap-1 ">
+          <div className="flex gap-1">
             <input
               type="text"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               placeholder="Écrire un message"
-              className="flex-grow border border-gray-300 rounded-xl p-2 focus:outline-none focus:border-red-500 shadow-inner"
+              className="flex-grow border border-gray-300 rounded-xl p-2 min-w-0 focus:outline-none focus:border-red-500 shadow-inner"
 
             />
-            <button type="button" onClick={sendMessage} className="transform hover:scale-110" > <ChatSendIcon /> </button>
+            <button type="button" onClick={sendMessage} className="transform hover:scale-105 " > <ChatSendIcon /> </button>
           </div>
         </div>
       </div>
