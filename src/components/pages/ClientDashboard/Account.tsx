b3b0f919e3ad4881176d7a2deb086d07c413e260
silverknight59222 <silverknight59222@gmail.com>
2023-09-28 11:24:42 +0200

    ClientAccount: chapters have their card modify button leads to popup popup not finished ! confidentiality page -> login issue

--- src/components/pages/ClientDashboard/Account.tsx
+++ src/components/pages/ClientDashboard/Account.tsx
@@ -1,6 +1,10 @@
 import { CircleRight } from "@/components/utilis/Icons";
 import ClientDashboardLayout from "@/layout/ClientDashboardLayout";
 import React, { useState } from "react";
+import BaseModal from "@/components/UI/BaseModal";
+import Footer from "@/components/UI/Footer";
+import { ColorsThemeA, Theme_A } from "@/components/utilis/Themes";
+
 
 const Account = () => {
     const [selectedTab, setSelectedTab] = useState(0);
@@ -13,35 +17,204 @@ const Account = () => {
         "Confidentialité et règles d’utilisation",
         "Langue",
     ];
+
+
+    ////////////////////////////////////////////////////
+    ///////////////////// PASSWORD 
+    ////////////////////////////////////////////////////
+    const [passwordField, renewPassword] = useState({
+        old: "",
+        new: "",
+        new2: "",
+    });
+    const setOldPassword = (value: string) => {
+        renewPassword((prev) => {
+            return { ...prev, old: value };
+        });
+    };
+    const setNewPassword = (value: string) => {
+        renewPassword((prev) => {
+            return { ...prev, new: value };
+        });
+    };
+    const setNew2Password = (value: string) => {
+        renewPassword((prev) => {
+            return { ...prev, new2: value };
+        });
+    };
+
+    const onSubmit = async () => {
+        if (passwordField.new != passwordField.new2) { // TODO modify
+            setError((prev) => {
+                return { ...prev, text: "Nouveaux mot de passes différents" };
+            });
+            return;
+        }
+        else if (passwordField.new.length < 8) {
+            setError((prev) => {
+                return { ...prev, text: "Nouveaux mot de passes trop petit" };
+            });
+            return;
+        }
+        else {
+            setError((prev) => {
+                return { ...prev, text: "" };
+            });
+        }
+    }
+
+    const [error, setError] = useState({
+        text: "",
+    })
+    function modifPassWord() {
+        return (
+            <div>
+                <div className="flex flex-col items-center justify-center gap-4">
+                    <p className="text-3xl font-semibold text-black text-center">Modification du mot de passe</p>
+
+
+                    {error && (
+                        <p className={`${Theme_A.checkers.errorText}`}>
+                            {error.text}
+                        </p>
+                    )}
+                    <input
+                        // type=""
+                        placeholder="Ancien mot de passe"
+                        className="w-full p-3 placeholder:text-[#959595] placeholder:text-base rounded-md shadow-[0px_4px_23px_0px_rgba(193,193,193,0.25)] outline-none"
+                        value={passwordField.old}
+                        maxLength={30}
+                        onChange={(e) => setOldPassword(e.target.value)}
+                    />
+                    <input
+                        // type=""
+                        placeholder="Nouveau mot de passe"
+                        className="w-full p-3 placeholder:text-[#959595] placeholder:text-base rounded-md shadow-[0px_4px_23px_0px_rgba(193,193,193,0.25)] outline-none"
+                        value={passwordField.new}
+                        maxLength={30}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                    />
+
+                    <input
+                        // type=""
+                        placeholder="Nouveau mot de passe"
+                        className="w-full p-3 placeholder:text-[#959595] placeholder:text-base rounded-md shadow-[0px_4px_23px_0px_rgba(193,193,193,0.25)] outline-none"
+                        value={passwordField.new2}
+                        maxLength={30}
+                        onChange={(e) => setNew2Password(e.target.value)}
+                    />
+                </div>
+                <div className="mt-4 flex gap-4 items-center justify-center w-full">
+                    <button
+                        className={`${Theme_A.button.medWhiteColoredButton}`}
+                        onClick={() => null} // TODO
+                    >
+                        Annuler
+                    </button>
+                    <button
+                        className={`${Theme_A.button.mediumGradientButton}`}
+                        onClick={() => onSubmit()}
+                    >
+                        Actualiser
+                    </button>
+                </div>
+            </div>
+        )
+    };
+
     const informations = [
-        { name: "Nom légal", desc: "Dimitri Bala" },
-        { name: "Adresse", desc: "Information non fournie" },
-        { name: "Numéro de téléphone", desc: "+41 ** *** 62 92" },
-        { name: "Adresse e-mail", desc: "b***9@gmail.com" },
-        { name: "Pièce d'identité officielle", desc: "Information non fournie" },
-        { name: "Statut", desc: "Etudiant - vérifié" },
+        { name: "Nom légal", desc: "Dimitri Bala", modif: false },
+        { name: "Adresse", desc: "Information non fournie", modif: true },
+        { name: "Numéro de téléphone", desc: "+41 ** *** 62 92", modif: true },
+        { name: "Adresse e-mail", desc: "b***9@gmail.com", modif: false },
+        { name: "Pièce d'identité officielle", desc: "Information non fournie", modif: true },
+        { name: "Statut", desc: "Etudiant - vérifié", modif: true },
     ];
+
+    const password = [
+        { name: "Mot de passe", desc: "", modif: true },
+    ];
+
     const notifications = [
-        { name: "Activité du compte", desc: "Activé : E-mail et SMS" },
-        { name: "Rappels", desc: "Activé : E-mail, SMS et Whatsapp" },
-        { name: "Messages", desc: "Activé : E-mail, Whatsapp" },
+        { name: "Activité du compte", desc: "Activé : E-mail et SMS", modif: true },
+        { name: "Rappels", desc: "Activé : E-mail, SMS et Whatsapp", modif: true },
+        { name: "Messages", desc: "Activé : E-mail, Whatsapp", modif: true },
     ];
+
+    const payments = [
+        { name: "Carte de crédit", desc: "Enregistrée", modif: true },
+        { name: "Paypal", desc: "Bientôt disponible", modif: false },
+    ];
+
+    const reservations = [
+        { name: "", desc: "", modif: false },
+    ];
+
+    const confidentiality = [
+        { name: "", desc: "", modif: false },
+    ];
+
+    const languages = [
+        { name: "Langue actuelle", desc: "Francais", modif: true },
+    ];
+
+
     const [showItem, setShowItem] = useState(informations);
+    const [selectedItem, setSelectedItem] = useState(null);
     const onSelectTab = (item: string, index: number) => {
         setSelectedTab(index);
         if (item === "Notifications") {
             setShowItem(notifications);
-        } else {
+        }
+        else if (item === "Moyens de paiements") {
+            setShowItem(payments);
+        }
+        else if (item === "Langue") {
+            setShowItem(languages);
+        }
+        else if (item === "Mot de passe") {
+            setShowItem(password);
+        }
+        else if (item === "Réservations en cours") {
+            setShowItem(reservations);
+        }
+        else if (item === "Confidentialité et règles d’utilisation") {
+            setShowItem(confidentiality);
+            const urlToOpen = '/confidentiality'; // Replace with your URL or component path
+            // Open the URL in a new tab
+            window.open(urlToOpen);
+        }
+        else {
             setShowItem(informations);
         }
     };
+
+    const [isModal, setIsModal] = useState(false);
+    const handleModifierClick = (item: any) => {
+        setIsModal(true); // Show the modal when "modifier" is clicked
+        setSelectedItem(item);
+    };
+
+
+
+
     return (
         <div>
-            <div className="hidden lg:block fixed -right-32 md:-right-28 -bottom-32 md:-bottom-28 z-10">
+            <div className="hidden lg:block fixed -right-32 md:-right-28 -bottom-32 md:-bottom-28 -z-10">
                 <CircleRight />
             </div>
             <ClientDashboardLayout>
                 <div className="mt-14 mb-5 px-6">
+                    /* POPUP */
+                    {isModal && (
+                        <BaseModal close={() => setIsModal(!isModal)}>
+                            <div>
+                                {modifPassWord()}
+                            </div>
+
+                        </BaseModal>)}
+
+                    {/* NORMAL VIEW */}
                     <p className="text-black font-medium text-3xl text-center">
                         Gestion du compte
                     </p>
@@ -60,7 +233,7 @@ const Account = () => {
                                 );
                             })}
                         </div>
-                        <div className="relative z-10 w-full md:w-6/12 md:h-[590px] mt-5 md:mt-0 rounded-3xl bg-white py-8 px-14 shadow-[0px_13px_37px_0px_rgba(176,176,176,0.28)]">
+                        <div className=" w-full md:w-6/12 md:h-[590px] mt-5 md:mt-0 rounded-3xl bg-white py-8 px-14 shadow-[0px_13px_37px_0px_rgba(176,176,176,0.28)]">
                             {showItem.map((item, index) => {
                                 return (
                                     <div
@@ -71,15 +244,23 @@ const Account = () => {
                                             <p className="text-black">{item.name}</p>
                                             <p className="text-[#666] text-sm">{item.desc}</p>
                                         </div>
-                                        <div className="text-black underline text-xs">modifier</div>
+                                        {item.modif === true ?
+                                            <div className="cursor-pointer text-black underline text-xs"
+                                                onClick={() => handleModifierClick(item)}
+                                            >modifier
+                                            </div>
+                                            :
+                                            <div></div>
+                                        }
                                     </div>
                                 );
                             })}
                         </div>
                     </div>
                 </div>
-            </ClientDashboardLayout>
-        </div>
+            </ClientDashboardLayout >
+            <Footer />
+        </div >
     )
 }
 
---
+++ src/components/shared/confidentiality.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const confidentialityPage = () => {
+  return (
+    <div>
+      <h1>Confidentialités</h1>
+      <p>
+        This is the standard text that will be displayed in the component.
+        You can replace this text with your own content as needed.
+      </p>
+    </div>
+  );
+};
+
+export default confidentialityPage;
