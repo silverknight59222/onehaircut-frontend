fd2394b7550c31959d860e8a001f82980d26ad03
silverknight59222 <silverknight59222@gmail.com>
2023-10-07 20:18:44 +0200

    ChooseSalon Update - almost done - need data

--- src/components/pages/ChooseSalon/index.tsx
+++ src/components/pages/ChooseSalon/index.tsx
@@ -12,8 +12,14 @@ import { SalonDetails } from '@/types';
 import userLoader from "@/hooks/useLoader";
 import useSnackbar from '@/hooks/useSnackbar';
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
-import { Theme_A } from '@/components/utilis/Themes';
+import { ColorsThemeA, Theme_A } from '@/components/utilis/Themes';
 import { BackArrow } from '@/components/utilis/Icons';
+import Footer from '@/components/UI/Footer';
+import MapIcon from "@/components/utilis/Icons";
+import ReactDOMServer from 'react-dom/server';
+// TODO IMPORT TO USE ADRESSES 
+//import axios from 'axios'; 
+
 
 
 // Composant principal SalonChoice
@@ -30,7 +36,7 @@ const SalonChoice = () => {
     const [isLoading, setIsLoading] = useState(false);
     const { loadingView } = userLoader();
     const showSnackbar = useSnackbar();
-    const [location, setLocation] = useState({ lat: 48.8584, lng: 2.2945 });
+    const [location, setLocation] = useState({ lat: 47.18052966583263, lng: 7.358082527907601 });
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [wishlist, setWishlist] = useState<string[]>([])
     const { isLoaded } = useJsApiLoader({
@@ -149,92 +155,176 @@ const SalonChoice = () => {
         return loadingView()
     }
 
+    //TODO UNCOMMENT TO USE SALON ADRESSES ( NOT LAT AND LONG; FULL ADRESS)
+    // Pour utiliser l'adresse du salon 
+    /*
+     const getCoordinates = async (address: string) => {
+         try {
+             const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json`, {
+                 params: {
+                     address: address,
+                     key: 'VOTRE_CLÉ_API',
+                 }
+             });
+             if (response.data.status === 'OK') {
+                 return response.data.results[0].geometry.location;
+             }
+         } catch (error) {
+             console.error('Erreur de géocodage:', error);
+         }
+     };
+     useEffect(() => {
+         const getSalonCoordinates = async () => {
+             const updatedSalons = await Promise.all(salons.map(async (salon) => {
+                 const coords = await getCoordinates(salon.address);
+                 return { ...salon, coordinates: coords };
+             }));
+             setSalons(updatedSalons);
+         };
+ 
+         getSalonCoordinates();
+     }, [salons]);
+ */
+
+    // MARQUEUR PERSONNALISE
+    const mapIconSvg = ReactDOMServer.renderToStaticMarkup(<MapIcon />);
+    const mapIconUrl = `data:image/svg+xml;base64,${btoa(mapIconSvg)}`;
+
     // Rendu du composant
     return (
         <div className='w-full'>
-            {/* Entête/Navbar du composant */}
+            {/* Entête du composant */}
             <Navbar isSalonPage={true} />
+
+            {/* Corps du composant */}
             <div className='w-full flex flex-col items-center justify-center px-6'>
-                {/* Vérifier si le contenu est en cours de chargement et afficher le loader si nécessaire */}
                 {isLoading && loadingView()}
-                {/* Texte affichant le nombre de salons trouvés */}
-                <p className='text-4xl font-medium text-black text-center mt-14'>87 <span className='font-bold text-gradient'>Salons</span> correspondent à vos critères</p>
-                {/* <div className='flex flex-col md:flex-row items-center justify-center gap-8  mt-6'>
-                    <div onClick={() => setSelectedTab(0)} className='flex items-center justify-center gap-7 w-[350px] h-14 border border-[#BDBDBD] rounded-xl cursor-pointer'>
-                        <p className='text-xl font-semibold'>Selection d’une coiffure</p>
-                        {selectedTab === 0 ? <RegistrationCheckedIcon /> : <div className='w-7 h-7 bg-[#D9D9D9] rounded-full' />}
-                    </div>
-                    <div onClick={() => setSelectedTab(1)} className='flex items-center justify-center gap-7 w-[350px] h-14 border border-[#BDBDBD] rounded-xl cursor-pointer'>
-                        <p className='text-xl font-semibold'>Selection d’une prestation</p>
-                        {selectedTab === 1 ? <RegistrationCheckedIcon /> : <div className='w-7 h-7 bg-[#D9D9D9] rounded-full' />}
-                    </div>
-                </div> */}
-                <div className='w-full flex items-end justify-between mt-12 px-4'>
-                    {/* Bouton pour revenir aux services */}
-                    <div className='flex items-center cursor-pointer mt-10 mb-8 sm:mx-10 2xl:mx-14 text-stone-800' onClick={() => router.push('/services')}>
+
+                {/* Texte indiquant le nombre de salons */}
+                <p className='text-4xl font-medium text-black text-center mt-6'>
+                    {salons.length} <span className='font-bold text-gradient'>{salons.length === 1 ? 'Salon' : 'Salons'}</span> {salons.length === 1 ? 'correspond' : 'correspondent'} à vos critères
+                </p>
+
+                {/* Bouton retour et continuer */}
+                <div className='w-full flex items-end justify-between mt-4 px-4'>
+                    {/* Bouton pour retourner aux services */}
+                    <div className='flex items-center cursor-pointer mt-4 mb-2 sm:mx-10 2xl:mx-14 text-stone-800' onClick={() => router.push('/services')}>
                         <BackArrow />
                         <p className={`${Theme_A.textFont.navigationGreyFont}`}>Retour aux services</p>
                     </div>
-                    {/* Bouton pour continuer avec le salon sélectionné */}
-                    <button disabled={!selectedSalon.id} onClick={onContinue} className={`flex items-center justify-center text-lg text-white font-medium w-full md:w-52 h-14 rounded-xl px-4 ${selectedSalon.id ? Theme_A.button.medLargeGradientButton : 'bg-[#D9D9D9]'}`}>Continuer</button>
+
+                    {/* Bouton pour continuer */}
+                    <button disabled={!selectedSalon.id} onClick={onContinue}
+                        className={`flex items-center justify-center text-lg text-white font-medium w-full md:w-52 h-14 rounded-xl px-4 ${selectedSalon.id ? Theme_A.button.medLargeGradientButton : 'bg-[#D9D9D9]'}`}>
+                        Continuer
+                    </button>
                 </div>
 
+                {/***************************************************************************************************************************************************************************************************************** */}
 
-                {/* Section principale affichant les salons et la carte */}
-                <div className='w-full mt-14 mb-5'>
-                    <div className='w-full flex flex-col lg:flex-row items-start justify-center gap-6'>
-                        <div className='md:h-[1100px] md:overflow-y-auto'>
-                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 gap-6">
-                                {/* Mapping et affichage des salons */}
-                                {salons.map((salon, index) => {
-                                    return <div key={index} onClick={() => setSelectedSalon({ name: salon.name, id: salon.id })} className={`bg-[rgba(242,242,242,0.66)] rounded-2xl pb-3 border hover:border-secondary cursor-pointer ${selectedSalon.id === salon.id && 'border-secondary'}`}>
-                                        <div className="px-4 pt-4 relative">
+                {/* Conteneur principal pour les salons et la carte */}
+                <div className='w-full mt-4 mb-2 relative'>
+
+                    {/* Carte Google affichée uniquement si des salons sont disponibles */}
+                    {
+                        salons.length > 0 && (
+                            <div className={`lg:absolute lg:top-0 lg:left-0 w-full h-[400px] lg:w-[400px] lg:h-[970px] 2xl:w-[920px] 4xl:w-[920px] rounded-lg overflow-hidden lg:z-10`}>
+
+                                <GoogleMap
+                                    center={location}
+                                    zoom={12}
+                                    mapContainerStyle={{ width: '100%', height: '100%' }}
+                                >
+                                    {salons.map((salon, index) => (
+                                        <Marker
+                                            key={index}
+                                            //TODO UTILISER LA LAT-LONG DU SALON => position={{ lat: salon.latitude, lng: salon.longitude }} or 
+                                            //TODO UTILISER L'ADRESSE DU SALON => position={salon.coordinates}
+                                            label={{
+                                                color: '#000',  // Couleur du texte
+                                                fontFamily: 'Arial', // Famille de la police
+                                                fontSize: '10px',  // Taille de la police
+                                                fontWeight: 'semibold', // Poids de la police
+                                                text: `${salon.id}00$`, // Texte à afficher
+                                            }}
+                                            position={location}
+                                            icon={{
+                                                url: mapIconUrl, // URL de l'icône personnalisée
+                                                scaledSize: new window.google.maps.Size(70, 90), // Taille de l'icône
+                                                origin: new window.google.maps.Point(0, -10), // Point d'origine de l'image
+                                                anchor: new window.google.maps.Point(20, 40), // Point d'ancrage de l'icône
+                                            }}
+                                        />
+                                    ))}
+                                </GoogleMap>
+                            </div>
+                        )
+                    }
+
+                    {/* Section affichant les vignettes des salons */}
+                    <div className='flex-1 pr-4 pb-4 overflow-y-auto h-[calc(100vh - 160px)] lg:relative lg:mt-0 lg:ml-[410px] 2xl:ml-[930px] 4xl:ml-[930px]'>
+
+                        {/* Grid contenant les vignettes */}
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pb-10">
+
+                            {/* VIGNETTES (ITERATIONS) */}
+                            {salons.map((salon, index) => (
+                                <div
+                                    key={index}
+                                    onClick={() => setSelectedSalon({ name: salon.name, id: salon.id })}
+                                    className={`relative bg-stone-100 rounded-2xl border hover:border-secondary cursor-pointer ${selectedSalon.id === salon.id && 'border-secondary'}`}
+                                    style={{ width: '100%', aspectRatio: '1/1', display: 'flex', flexDirection: 'column', minWidth: '300px', minHeight: '300px' }}
+                                >
+                                    {/* Contenu de la vignette */}
+                                    <div className="flex flex-col p-4 shadow-md rounded-2xl " style={{ flexGrow: 1 }}>
+
+                                        <div className='relative mb-4 ' style={{ flexGrow: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                                             {!isLoggedIn &&
                                                 <div onClick={(e) => onWishlist(e, salon.id)} className="absolute right-6 top-6 z-20 cursor-pointer">
                                                     <Like color={wishlist.includes(String(salon.id)) ? "#FF0000" : ""} />
                                                 </div>}
-                                            <div className='relative w-48 h-48'>
-                                                <Image
-                                                    src={salon.salon_images.length && salon.salon_images[index].is_cover ? salon.salon_images[index].image.includes('api-server') ? salon.salon_images[index].image : `https://api-server.onehaircut.com/public${salon.salon_images[index].image}` : salon.logo.includes('api-server') ? salon.logo : `https://api-server.onehaircut.com/public${salon.logo}`}
-                                                    fill={true}
-                                                    alt="image"
-                                                />
-                                            </div>
+                                            <Image
+                                                src={salon.salon_images.length && salon.salon_images[index].is_cover ? salon.salon_images[index].image.includes('api-server') ? salon.salon_images[index].image : `https://api-server.onehaircut.com/public${salon.salon_images[index].image}` : salon.logo.includes('api-server') ? salon.logo : `https://api-server.onehaircut.com/public${salon.logo}`}
+                                                fill={true}
+                                                alt="image"
+                                                style={{ objectFit: 'cover', height: '100%', width: '100%', display: 'block' }}
+                                                className="rounded-2xl "
+                                            />
                                         </div>
-                                        <div className="flex items-start justify-between text-black text-lg font-semibold px-3 pt-2">
+
+                                        {/* Nom et prix du salon */}
+                                        <div className="flex items-start justify-between text-black text-lg font-semibold px-3 pt-2 ">
                                             <p className='w-36'>{salon.name}</p>
-                                            <p>$35</p>
+                                            <p className={`p-2 ${ColorsThemeA.OhcGradient_B} rounded-full border border-stone-300 text-white`}> $35</p>
                                         </div>
+
+                                        {/* Évaluation et nombre d'avis */}
                                         <div className='flex items-center gap-1 text-xs text-[#7B7B7B] px-3 pt-1'>
                                             <StarRatings
-                                                rating={1}
+                                                rating={salon.rating}
                                                 starRatedColor="#FEDF10"
                                                 starSpacing="4px"
                                                 starDimension="12px"
-                                                numberOfStars={1}
+                                                numberOfStars={5}
                                                 name="rating"
                                             />
                                             <p className='border-r border-[#A7A7A7] pr-1'>{salon.rating}</p>
                                             <p>348 avis</p>
                                         </div>
                                     </div>
-                                })}
-                            </div>
+                                </div>
+                            ))}
+
+
+
                         </div>
-                        {/* Carte Google Maps affichée seulement si des salons sont présents */}
-                        {salons.length ?
-                            <div className='hidden lg:block w-[300pxw] lg:w-[400px] 2xl:w-[725px]'>
-                                <GoogleMap center={location} zoom={10} mapContainerStyle={{ width: '100%', height: '100vh' }}>
-                                    <Marker position={location} />
-                                </GoogleMap>
-                            </div>
-                            : ''
-                        }
                     </div>
                 </div>
             </div>
+            {/* Pied de page du composant */}
+            <Footer />
         </div>
-    )
-}
+    );
+};
 
-export default SalonChoice
+export default SalonChoice;
