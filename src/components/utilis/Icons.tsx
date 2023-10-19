e6faee2e2999e4e20491deecb833a72ec5f136c4
silverknight59222 <silverknight59222@gmail.com>
2023-10-19 12:20:25 +0200

    strokeWidth fixed

--- src/components/utilis/Icons.tsx
+++ src/components/utilis/Icons.tsx
@@ -2937,7 +2937,7 @@ export const EyeIcon = () => {
 export const TotheTopIcon = () => {
   return (
     <svg className="w-6 h-6 text-gray-700 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
-      <path stroke="currentColor" stroke-linecap="round" strokeLinejoin="round" stroke-width="2" d="M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7" />
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7" />
     </svg>
   );
 };
