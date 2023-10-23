7a28c1322ad4469985d5af52cc07772947accb9d
silverknight59222 <silverknight59222@gmail.com>
2023-10-23 18:16:56 +0200

    Salon settings design update - 5% - Need linkage

---
+++ src/components/pages/Settings/SalonInfos.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import BaseModal from "@/components/UI/BaseModal";
+import DropdownMenu from "@/components/UI/DropDownMenu";
+
+const SalonInfos = () => {
+    const [showModal, setShowModal] = useState(false); // Initialize showModal state
+
+    return (
+        <div className={`w-[400px] h-max bg-white rounded-2xl py-4 shadow-lg`}>
+            {showModal && (
+                <BaseModal close={() => setShowModal(false)}>
+                    {/* Add content for the modal here */}
+                    <p>This is the modal content.</p>
+                </BaseModal>
+            )}
+        </div>
+    );
+};
+
+export default SalonInfos;
