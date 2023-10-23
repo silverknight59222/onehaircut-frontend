fb0b5afa83cc69d54cb45aa82b5a2c228705ddfd
silverknight59222 <silverknight59222@gmail.com>
2023-10-24 00:43:17 +0200

    Salon settings design update 10% - need linkage

--- src/components/pages/Settings/SalonInfos.tsx
+++ src/components/pages/Settings/SalonInfos.tsx
@@ -1,18 +1,63 @@
-import React, { useEffect, useState } from "react";
-import BaseModal from "@/components/UI/BaseModal";
-import DropdownMenu from "@/components/UI/DropDownMenu";
+import React, { useState } from "react";
+import AdressModal from "./AddressModal";
+import { EditIcon } from "@/components/utilis/Icons";
+import { Theme_A } from "@/components/utilis/Themes";
 
 const SalonInfos = () => {
-    const [showModal, setShowModal] = useState(false); // Initialize showModal state
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+    };
 
     return (
-        <div className={`w-[400px] h-max bg-white rounded-2xl py-4 shadow-lg`}>
+        <div className={`w-[500px] h-max bg-white rounded-2xl py-4 shadow-lg`}>
             {showModal && (
-                <BaseModal close={() => setShowModal(false)}>
-                    {/* Add content for the modal here */}
-                    <p>This is the modal content.</p>
-                </BaseModal>
+                <AdressModal close={() => setShowModal(false)}>
+                    <p>Ceci est le contenu du modal.</p>
+                </AdressModal>
             )}
+
+
+
+            {/* Choix du pays */}
+            <div className="flex">
+                {/* Ajouter un Dowpdown pour selectionner le paysd*/}
+
+                {/* ADRESSE DE L'ÉTABLISSEMENT */}
+                <div className="flex-1 py-5 pl-5 overflow-hidden">
+                    <ul>
+                        <li className="text-xs text-gray-800 uppercase font-semibold ">Adresse de l'établissement</li>
+                        <li>Max Mustermann</li>
+                        <li>Musterstrasse 1</li>
+                        <li>4020 Linz</li>
+                        <li>Suisse</li>
+                    </ul>
+                </div>
+                {/* ADRESSE DE FACTURATION */}
+                <div className="flex-1 py-5 pl-1 overflow-hidden">
+                    <ul>
+                        <li className="text-xs text-gray-800 uppercase font-semibold">Adresse de facturation</li>
+                        <li>Rick Astley</li>
+                        <li>Rickrolled 11</li>
+                        <li>1000 Vienna</li>
+                        <li>Autriche</li>
+                    </ul>
+                </div>
+
+                {/* EDIT LES ADRESSES*/}
+                <div
+                    className={`${Theme_A.servicesCards.modifyButton} mr-4 shadow-md  transition-transform duration-300 transform hover:scale-125`}
+                    onClick={openModal}
+                >
+                    <EditIcon />
+                </div>
+            </div>
+
         </div>
     );
 };
