2a948cd5dffe8f4d948d8ba7d2606aeb09d0ae52
silverknight59222 <silverknight59222@gmail.com>
2023-10-24 15:56:13 +0200

    Salon Settings - 20% - need linkage

--- src/components/pages/Settings/SalonInfos.tsx
+++ src/components/pages/Settings/SalonInfos.tsx
@@ -1,26 +1,68 @@
 import React, { useState } from "react";
-import AdressModal from "./AddressModal";
 import { EditIcon } from "@/components/utilis/Icons";
 import { Theme_A } from "@/components/utilis/Themes";
+import BaseModal from "@/components/UI/BaseModal";
 
 const SalonInfos = () => {
-    const [showModal, setShowModal] = useState(false);
-
+    const [isModal, setIsModal] = useState(false)
     const openModal = () => {
-        setShowModal(true);
+        setIsModal(true);
     };
-
     const closeModal = () => {
-        setShowModal(false);
+        setIsModal(false);
     };
 
     return (
         <div className={`w-[500px] h-max bg-white rounded-2xl py-4 shadow-lg`}>
-            {showModal && (
-                <AdressModal close={() => setShowModal(false)}>
-                    <p>Ceci est le contenu du modal.</p>
-                </AdressModal>
-            )}
+            {isModal &&
+                <BaseModal close={() => setIsModal(false)}>
+                    <div className="relative z-100"> {/* z-50 or more to make sure it is always on top */}
+                        {/* Contenu du Modal Adresse */}
+
+                        <div className="flex">
+                            <div className="flex-1 py-5 pl-5 overflow-hidden">
+                                <h1 className="inline text-2xl font-semibold leading-none">Adresse</h1>
+                            </div>
+                        </div>
+                        <div className="px-5 pb-5">
+                            <input placeholder="Name" className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400" /><input placeholder="Address" className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400" />
+                            <div className="flex">
+                                <div className="flex-grow w-1/4 pr-2"><input placeholder="PLZ" className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400" /></div>
+                                <div className="flex-grow"><input placeholder="City" className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400" /></div>
+                            </div>
+                            <div className="flex items-center pt-3"><input type="checkbox" className="w-4 h-4 text-black bg-gray-300 border-none rounded-md focus:ring-transparent" /><label htmlFor="safeAdress" className="block ml-2 text-sm text-gray-900">Adresse de facturation</label></div>
+                        </div>
+                        <div className="flex">
+                            <div className="flex-1 py-5 pl-5 overflow-hidden">
+                                <h1 className="inline text-2xl font-semibold leading-none">Adresse de facturation</h1>
+                            </div>
+                            <div className="flex-none pt-2.5 pr-2.5 pl-1" />
+                        </div>
+                        <div className="px-5 pb-5">
+                            <input placeholder="Name" className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400" /><input placeholder="Address" className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400" />
+                            <div className="flex">
+                                <div className="flex-grow w-1/4 pr-2"><input placeholder="PLZ" className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400" /></div>
+                                <div className="flex-grow"><input placeholder="City" className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200  focus:border-blueGray-500 focus:bg-white dark:focus:bg-gray-800 focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400" /></div>
+                            </div>
+                        </div>
+                        <hr className="mt-4" />
+                        <div className="flex flex-row-reverse p-3">
+                            <div className="flex-initial pl-3">
+                                <button type="button" className="flex items-center px-5 py-2.5 font-medium tracking-wide text-white capitalize   bg-black rounded-md hover:bg-gray-800  focus:outline-none focus:bg-gray-900  transition duration-300 transform active:scale-95 ease-in-out">
+                                    <span className="pl-2 mx-1">Enregistrer</span>
+                                </button>
+                            </div>
+                            <div className="flex-initial">
+                                <button type="button" className="flex items-center px-5 py-2.5 font-medium tracking-wide text-black capitalize rounded-md  hover:bg-red-200 hover:fill-current hover:text-red-600  focus:outline-none  transition duration-300 transform active:scale-95 ease-in-out">
+                                    <span className="pl-2 mx-1">Annuler</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Fin du contenu du modal */}
+                </BaseModal>
+            }
 
 
 
