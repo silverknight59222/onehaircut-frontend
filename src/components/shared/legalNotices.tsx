00656ffec5c52989b20cb35faa712ed54df555c2
silverknight59222 <silverknight59222@gmail.com>
2023-10-28 20:32:36 +0200

    Created Terms of services and legal notices pages

--- src/components/shared/legalNotices.tsx
+++ src/components/shared/legalNotices.tsx
@@ -6,30 +6,30 @@ import Footer from "@/components/UI/Footer";
 import { LogoCircleFixRight } from "@/components/utilis/Icons";
 import { Theme_A } from '../utilis/Themes';
 
-const termsPage = () => {
-  const router = useRouter();
+const legalNoticesPage = () => {
+    const router = useRouter();
 
-  return (
-    <div>
-      <div className="hidden lg:block fixed -right-32 md:-right-28 -bottom-32 md:-bottom-28 -z-10">
-        <LogoCircleFixRight />
-      </div>
-      <Footer />
-      <div className="flex justify-between w-full p-5 border-b border-[#EBF0F2]">
-        <div className='flex items-center justify-center cursor-pointer'
-          onClick={() => router.push('/')}>
-          <LogoIcon />
-        </div>
-        <div className="flex items-center justify-end gap-4">
-          <div className="w-14 h-14 flex items-center justify-center pb-1 border-2 border-secondary rounded-full cursor-pointer transform hover:scale-110 transition-transform"
-            onClick={() => router.push('/login')}>
-            <UserIcon />
-          </div>
-        </div>
-      </div>
-      <div className='mx-48 mb-16'>
-        <h1 className={`my-12 ${Theme_A.textFont.headerH1}`}>Conditions Générales d'Utilisation de OneHaircut</h1>
-        <p className='pb-6'> Les présentes Conditions Générales d'Utilisation (ci-après dénommées "CGU") régissent l'utilisation du site web de [Nom de l'entreprise] (ci-après dénommé "le Site") et de tous les services qui y sont proposés. En utilisant ce Site, vous acceptez de vous conformer à ces CGU. Si vous n'êtes pas d'accord avec ces conditions, veuillez ne pas utiliser le Site.</p>
+    return (
+        <div>
+            <div className="hidden lg:block fixed -right-32 md:-right-28 -bottom-32 md:-bottom-28 -z-10">
+                <LogoCircleFixRight />
+            </div>
+            <Footer />
+            <div className="flex justify-between w-full p-5 border-b border-[#EBF0F2]">
+                <div className='flex items-center justify-center cursor-pointer'
+                    onClick={() => router.push('/')}>
+                    <LogoIcon />
+                </div>
+                <div className="flex items-center justify-end gap-4">
+                    <div className="w-14 h-14 flex items-center justify-center pb-1 border-2 border-secondary rounded-full cursor-pointer transform hover:scale-110 transition-transform"
+                        onClick={() => router.push('/login')}>
+                        <UserIcon />
+                    </div>
+                </div>
+            </div>
+            <div className='mx-48 mb-16'>
+                <h1 className={`my-12 ${Theme_A.textFont.headerH1}`}>Mentions légales </h1>
+                {/* <p className='pb-6'> Les présentes Conditions Générales d'Utilisation (ci-après dénommées "CGU") régissent l'utilisation du site web de [Nom de l'entreprise] (ci-après dénommé "le Site") et de tous les services qui y sont proposés. En utilisant ce Site, vous acceptez de vous conformer à ces CGU. Si vous n'êtes pas d'accord avec ces conditions, veuillez ne pas utiliser le Site.</p>
         <h1 className={`my-3 ${Theme_A.textFont.headerH4}`}>
           1. Acceptation des Conditions</h1>
         <p className='pb-6'>
@@ -67,12 +67,12 @@ const termsPage = () => {
 
         <p className='pt-24'>
           Cette politique de confidentialité a été mise à jour le [date de la dernière révision].
-        </p>
+        </p> */}
 
-      </div>
+            </div>
 
-    </div>
-  );
+        </div>
+    );
 };
 
-export default termsPage;
+export default legalNoticesPage;
