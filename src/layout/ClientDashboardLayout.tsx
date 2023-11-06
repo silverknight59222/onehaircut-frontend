d6d97b3a5644929e3e95f4b5fbd87d6ed3ea0476
silverknight59222 <silverknight59222@gmail.com>
2023-11-06 21:10:40 +0100

    Cabine: updating Icon

--- src/layout/ClientDashboardLayout.tsx
+++ src/layout/ClientDashboardLayout.tsx
@@ -29,7 +29,7 @@ const ClientDashboardLayout = ({ children }: DashboardLayout) => {
     { icon: "MessageIcon", title: "Message", route: "/client/messages" },
     { icon: "StarGreyIcon", title: "Favoris", route: "/client/favorites" },
     { icon: "PortraitIcon", title: "Portrait", route: "/client/portrait" },
-    { icon: "PortraitIcon", title: "Cabine d'essayage", route: "/client/processed" },
+    { icon: "CabineIcon", title: "Cabine d'essayage", route: "/client/processed" },
     //TODO MODIFY MESSAGEICON VY FILTER ICON
     { icon: "MessageIcon", title: "Filtres", route: "/client/filters" },
     { icon: "ReservationIcon", title: "Réservations en cours", route: "/client/currentreservation" },
