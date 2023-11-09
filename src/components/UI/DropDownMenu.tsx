608860af2ed0e8c972f009b14c968819e4e5a3b9
silverknight59222 <silverknight59222@gmail.com>
2023-11-09 01:20:05 +0100

    DropDownMenu design back

--- src/components/UI/DropDownMenu.tsx
+++ src/components/UI/DropDownMenu.tsx
@@ -1,59 +1,132 @@
-import React, { useState } from 'react';
-// import { Menu, Transition } from 'react'
-import { DownArrow } from "../utilis/Icons";
-import { ColorsThemeA, Theme_A } from '../utilis/Themes';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
 
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        minWidth: 200,
+        marginBottom: theme.spacing(2),
+        border: '2px solid ' + theme.palette.grey[500],
+        textAlign: 'center',
+        borderRadius: '10px',
+        '& .MuiInput-underline:before': {
+            display: 'none',
+        },
+        '& .MuiInput-underline:after': {
+            display: 'none',
+        },
+        '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
+            display: 'none',
+        },
+        '&:hover .MuiOutlinedInput-notchedOutline': {
+            border: 'none',
+        },
+        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            border: 'none',
+        },
+    },
+    inputLabel: {
+        color: theme.palette.grey[500],
+        marginLeft: theme.spacing(1),
+        "&.Mui-focused": {
+            color: theme.palette.grey[500]
+        }
+    },
+    menuItem: {
+        fontSize: '0.875rem',
+        textAlign: 'center',
+        '&:hover': {
+            backgroundColor: theme.palette.grey[200],
+            borderRadius: '10px',
+        },
+        '&$selected': {
+            background: 'linear-gradient(35deg, #FFE190, #FFF7E1)', // This is a gradient from #E88018 to #FFB040 at a 45-degree angle
+            borderRadius: '10px',
+            fontWeight: '600',
+            margin: theme.spacing(1, 1),
+            color: '#3D3D3D',
+        },
+
+        paddingLeft: theme.spacing(2),
+        paddingRight: theme.spacing(0.5),
+        margin: theme.spacing(1, 1),
+    },
+    select: {
+        '& .MuiMenu-paper': {
+            borderRadius: '25px',
+        },
+    },
+    selected: {},
+}));
 
 const DropdownMenu = ({
     dropdownItems = [""],
-    showDefaultMessage = true, // showDefaultMessage defaults to false if not passed
-    width = '',
-    height = '',
-    rounded = '',
-    borderClr = '',
-    disabled = false,
-    backgroundClr = '',
-    fctToCallOnClick = (item:string) =>{}, // Function to be called when user clicks on an item
+    menuName = '',
+    fctToCallOnClick = (item: string) => { },
+    labelId = '',
+    selectId = '',
+    defaultSelected = '',
 }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [selectedItem, setSelectedItem] = useState(
-        showDefaultMessage || dropdownItems.length === 0 ? 'Select an option' : dropdownItems[0]
-    );
+    const classes = useStyles();
+    const [selectedItem, setSelectedItem] = useState('');
 
-    const toggleDropdown = () => {
-        if (!disabled) {
-            setIsOpen(!isOpen);
-        }
+    const handleChange = (item: any) => {
+        setSelectedItem(item);
+        fctToCallOnClick(item);
     };
-    const handleItemClick = (item: string) => {
-        setSelectedItem(item); // Update the selected item
-        setIsOpen(false); // Close the dropdown
-        fctToCallOnClick(item); // calling function from the other page
-    };
+
+    useEffect(() => {
+        setSelectedItem(selectId);
+    });
 
     return (
-        <div
-            className={`relative flex  items-center border-1 font-normal text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ${`${width ? width : 'w-40'} ${rounded ? rounded : 'rounded-md'} ${height ? height : 'h-8'} ${borderClr ? borderClr : 'border-black'} ${disabled ? 'bg-[rgba(171,171,171,0.10)] cursor-default' : backgroundClr ? `${backgroundClr} cursor-pointer` : 'bg-white cursor-pointer'}`}`}
-            onClick={toggleDropdown}>
-
-            <p className="text-sm p-2">{selectedItem}</p>
-            <div className="absolute right-3">
-                <DownArrow />
-            </div>
-
-            {isOpen && (
-                <div className={`absolute m-0 top-8 p-2 flex flex-col border-1 ${`${width ? width : 'w-40'} ${rounded ? rounded : 'rounded-md'}  ${borderClr ? borderClr : 'border-black'} ${disabled ? 'bg-[rgba(171,171,171,0.10)] cursor-default' : backgroundClr ? `${backgroundClr} cursor-pointer` : 'bg-white cursor-pointer'}`} `}
-                // style={{ width: width }}
+        <div>
+            <FormControl className={classes.formControl}>
+                <InputLabel
+                    id={labelId}
+                    className={classes.inputLabel}
                 >
-                    {dropdownItems.map((item) => (
-                        <p className='text-sm hover:bg-grey pl-2'
-                            key={item}
-                            onClick={() => handleItemClick(item)}>
+                    {menuName}
+                </InputLabel>
+                <Select
+                    labelId={labelId}
+                    id={labelId}
+                    value={selectedItem}
+                    onChange={(event) => handleChange(event.target.value)}
+                    className={classes.select}
+                    MenuProps={{
+                        anchorOrigin: {
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        },
+                        transformOrigin: {
+                            vertical: 'top',
+                            horizontal: 'left',
+                        },
+                        getContentAnchorEl: null,
+                        PaperProps: {
+                            style: {
+                                marginTop: '10px',
+                                borderRadius: '15px',  // for rounded modal corners
+                            }
+                        }
+                    }}
+                >
+                    {dropdownItems.map((item, index) => (
+                        <MenuItem
+                            key={index}
+                            value={item}
+                            className={classes.menuItem}
+                            classes={{ selected: classes.selected }}
+                        >
                             {item}
-                        </p>
+                        </MenuItem>
                     ))}
-                </div>
-            )}
+                </Select>
+            </FormControl>
         </div>
     );
 };
