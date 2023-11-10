8b8b7e925d88dea71f84e3db04ca7b53356ad7f7
silverknight59222 <silverknight59222@gmail.com>
2023-11-10 16:37:39 +0100

    InfoSalon - type d etablissement Icons updated

--- src/components/pages/Settings/SalonInfos.tsx
+++ src/components/pages/Settings/SalonInfos.tsx
@@ -40,133 +40,6 @@ const SalonInfos = () => {
         setIsModal(false);
     };
 
-    // Fonction pour générer les champs d'entrée Pour l'adresse standard
-    const renderTextInputField = (placeholder: string, type?: string) => (
-        <input
-            placeholder={placeholder}
-            type={type}
-            inputMode="text"
-            maxLength={50}
-            className="text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200 focus:border-Gray-500 focus:bg-gray-900 focus:text-white focus:placeholder-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
-            onInput={(e) => {
-                // Nothing to do on a single character change
-            }}
-        />
-    );
-
-    // Fonction pour générer les champs d'entrée Pour l'adresse standard
-    const renderZipField = (placeholder: string) => (
-        <input
-            placeholder={placeholder}
-            inputMode="numeric"
-            maxLength={5} // Limiter la longueur maximale à 5 caractères
-            className="text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200 focus:border-Gray-500 focus:bg-gray-900 focus:text-white focus:placeholder-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
-            onInput={(e) => {
-                let value = e.currentTarget.value;
-
-                // Remplacer tout ce qui n'est pas un chiffre par une chaîne vide
-                value = value.replace(/[^0-9]/g, '');
-
-                // Limiter la valeur à 5 caractères
-                if (value.length > 5) {
-                    value = value.slice(0, 5);
-                }
-
-                // Mettre à jour le code postal correspondant
-                setPostalCode(value);
-                e.currentTarget.value = value;
-            }}
-        />
-    );
-
-
-    // Fonction pour générer les champs d'entrée
-    const renderInputFieldBilling = (placeholder: string, type?: string) => (
-        <input
-            placeholder={placeholder}
-            type={type}
-            inputMode="numeric"
-            maxLength={5} // Limiter la longueur maximale à 5 caractères
-            className="text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200 focus:border-Gray-500 focus:bg-gray-900 focus:text-white focus:placeholder-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2 ring-gray-400"
-            onInput={(e) => {
-                let value = e.currentTarget.value;
-
-                // Remplacer tout ce qui n'est pas un chiffre par une chaîne vide
-                value = value.replace(/[^0-9]/g, '');
-
-                // Limiter la valeur à 5 caractères
-                if (value.length > 5) {
-                    value = value.slice(0, 5);
-                }
-
-                // Mettre à jour le code postal correspondant
-                setBillingPostalCode(value);
-                e.currentTarget.value = value;
-            }}
-        />
-    );
-
-    // Fonction pour générer un élément <li> avec style
-    const renderListItem = (label: string, text: string) => (
-        <li className="text-sm text-gray-400 italic">
-            {label} : {text}
-        </li>
-    );
-
-    // Fonction pour rechercher la ville en fonction du code postal
-    const searchCityByPostalCode = async (code: string) => {
-        try {
-            if (code.length === 5) {
-                const apiKey = 'AIzaSyAJiOb1572yF7YbApKjwe5E9L2NfzkH51E';
-                const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${code}&key=${apiKey}`);
-                const data = await response.json();
-                if (data.results.length > 0) {
-                    const cityComponent = data.results[0].address_components.find((component: any) =>
-                        component.types.includes('locality')
-                    );
-                    if (cityComponent) {
-                        if (isBillingAddressSame) {
-                            setBillingCity(cityComponent.long_name);
-                        } else {
-                            setCity(cityComponent.long_name);
-                        }
-                    } else {
-                        console.log('Ville non trouvée.');
-                    }
-                } else {
-                    console.log('Aucun résultat trouvé.');
-                }
-            }
-        } catch (error) {
-            console.error("Erreur lors de la recherche de la ville :", error);
-        }
-    };
-
-    // FOR AUTOMATICALLY FIND THE CITY OF BILLING ADDRESS 
-    const searchBillingCityByPostalCode = async (code: string) => {
-        try {
-            if (code.length === 5) {
-                const apiKey = 'AIzaSyAJiOb1572yF7YbApKjwe5E9L2NfzkH51E';
-                const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${code}&key=${apiKey}`);
-                const data = await response.json();
-                if (data.results.length > 0) {
-                    const cityComponent = data.results[0].address_components.find((component: any) =>
-                        component.types.includes('locality')
-                    );
-                    if (cityComponent) {
-                        setBillingCity(cityComponent.long_name);
-                    } else {
-                        console.log('Ville de facturation non trouvée.');
-                    }
-                } else {
-                    console.log('Aucun résultat trouvé.');
-                }
-            }
-        } catch (error) {
-            console.error("Erreur lors de la recherche de la ville de facturation :", error);
-        }
-    };
-
 
     // Utilisez useEffect pour déclencher la recherche de la ville lorsque le code postal change
     useEffect(() => {
@@ -174,22 +47,6 @@ const SalonInfos = () => {
         // searchCityByPostalCode(postalCode);
     }, []);
 
-    // Utilisez useEffect pour déclencher la recherche de la ville de facturation lorsque le code postal change
-    // useEffect(() => {
-    //     // searchBillingCityByPostalCode(billingPostalCode);
-    // }, []);
-
-
-
-    //For Dropdown lists Type of salon
-    const SalonTypeList = [
-        "Barber-shop",
-        "Salon pour hommes",
-        "Salon pour femmes",
-        "Coiffeur/euse mobile homme",
-        "Coiffeur/euse mobile femme",
-        "Salon mixte",
-    ];
     // handling the change of Salon type change
     const SaveSalonType = (item: string) => {
         // TODO: add backend to save the new preference
@@ -201,28 +58,52 @@ const SalonInfos = () => {
         setIsMobilityAllowed(event.target.checked);
     };
     const [selectedWeekday, setSelectedWeekday] = useState<string>('');
+
+
+    //For Dropdown lists Type of salon
+    const SalonTypeList = [
+        "Barber-shop",
+        "Salon pour hommes",
+        "Salon pour femmes",
+        "Coiffeur/euse mobile homme",
+        "Coiffeur/euse mobile femme",
+        "Salon mixte",
+    ];
+
+    const [selectedImageUrl, setSelectedImageUrl] = useState('/assets/DefaultPictures/Profil.png');
     const HandleSelectedSalonType = (item: string) => {
+        let imageUrl = '/assets/DefaultPictures/Profil.png';
         switch (item) {
             case 'Barber-shop':
                 setSalonType("Barber-shop");
+                imageUrl = "/assets/salon_types/BarberShop.png";
                 break;
             case 'Salon pour hommes':
                 setSalonType("Salon pour hommes");
+                imageUrl = "/assets/salon_types/Salon de coiffure pour homme.png";
                 break;
             case 'Salon pour femmes':
                 setSalonType("Salon pour femmes");
+                imageUrl = "/assets/salon_types/Salon de coiffure pour femme.png";
                 break;
             case 'Coiffeur/euse mobile homme':
                 setSalonType("Coiffeur/euse mobile homme");
+                imageUrl = "/assets/salon_types/Coiffeur Independant.png";
                 break;
             case 'Coiffeur/euse mobile femme':
                 setSalonType("Coiffeur/euse mobile femme");
+                imageUrl = "/assets/salon_types/Coiffeuse Indépendante.png";
                 break;
             case 'Salon mixte':
                 setSalonType("Salon mixte'");
+                imageUrl = "/assets/salon_types/Salon de coiffure mixte.png";
+                break;
+            default:
+                imageUrl = "/assets/salon_types/Salon de coiffure mixte.png";
                 break;
         }
         setSelectedSalonType(item);
+        setSelectedImageUrl(imageUrl);
     }
 
     //For the slider :
@@ -310,8 +191,8 @@ const SalonInfos = () => {
             billing_state: billingState,
             billing_country: billingCountry,
             is_billing_address_same: isBillingAddressSame,
-            latitude:locationLatitude,
-            longitude:locationLongitude
+            latitude: locationLatitude,
+            longitude: locationLongitude
         })
             .then((resp) => {
                 console.log(resp);
@@ -517,7 +398,7 @@ const SalonInfos = () => {
                                     type="button"
                                     onClick={
                                         SaveAddress
-                                        
+
                                     } // TODO SAVE ADDRESS
                                     className={`${Theme_A.button.medBlackColoredButton} ease-in-out transition duration-300`}
                                 >
@@ -623,10 +504,11 @@ const SalonInfos = () => {
                         selectId={SelectedSalonType}
                     />
                     <img
-                        src="chemin_de_votre_image.png"
-                        alt="Icon"
+                        src={selectedImageUrl}
+                        alt=""
                         className="w-32 h-24 mr-4 ml-8 bg-stone-300 rounded-lg mb-4 "
                     />
+
                 </div>
             </div>
 
