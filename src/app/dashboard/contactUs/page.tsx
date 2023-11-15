be66de324501a10283c3f9b776d91abb94b1e6ed
silverknight59222 <silverknight59222@gmail.com>
2023-11-15 22:38:41 +0100

    ContactUs page FE finished missing BE linkage

--- src/app/dashboard/contactUs/page.tsx
+++ src/app/dashboard/contactUs/page.tsx
@@ -4,8 +4,27 @@ import { LogoCircleFixRight } from '@/components/utilis/Icons';
 import { Theme_A } from '@/components/utilis/Themes';
 import DashboardLayout from '@/layout/DashboardLayout';
 import React, { useState } from "react";
+import DropdownMenu from "@/components/UI/DropDownMenu";
 
 const ContactUs = () => {
+
+  //For Dropdown lists 
+  const contactType = [
+    "Problème",
+    "Information",
+  ];
+
+  const [SelectedContactType, setSelectedContactType] = useState<string>('');
+  // for input text field
+  const [multilineText, setMultilineText] = useState('');
+
+  const handleMultilineChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMultilineText(event.target.value);
+  };
+
+  const onSend = () => {
+    // TODO send per email to us
+  };
 
   return (
     <div>
@@ -22,6 +41,30 @@ const ContactUs = () => {
             <p className="text-stone-500 italic font-normal text-lg text-center my-8">
               Vous voulez nous faire savoir quelque chose, rapporter un problème ou même nous encourager, c'est ici que ca se passe!
             </p>
+            <DropdownMenu
+              dropdownItems={contactType}
+              fctToCallOnClick={setSelectedContactType}
+              menuName="Type de contact"
+              selectId={SelectedContactType}
+            />
+            {SelectedContactType !== '' &&
+              <div className="flex flex-col items-center justify-center ">
+                <textarea
+                  id="multilineInput"
+                  value={multilineText}
+                  onChange={handleMultilineChange}
+                  className={`${Theme_A.fields.inputField2}`}
+                  rows={8} // Specify the number of visible rows
+                  cols={80} // Specify the number of visible columns
+                />
+                <button
+                  onClick={() => onSend()}
+                  disabled={multilineText == ''}
+                  className={` rounded-xl mt-6 w-2/5 h-16 ${multilineText == '' ? Theme_A.button.bigWhiteGreyButton : Theme_A.button.bigGradientButton} `}>
+                  Envoyer
+                </button>
+              </div>
+            }
           </div>
         </div>
       </DashboardLayout>
