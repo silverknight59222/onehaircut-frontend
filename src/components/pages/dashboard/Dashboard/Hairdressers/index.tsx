ffc1584be7f66de4781481274e09a2681e40d7ab
silverknight59222 <silverknight59222@gmail.com>
2023-11-26 13:39:51 +0100

    Salon Hairdresser - changing password design

--- src/components/pages/dashboard/Dashboard/Hairdressers/index.tsx
+++ src/components/pages/dashboard/Dashboard/Hairdressers/index.tsx
@@ -16,6 +16,7 @@ import { EditIcon, LogoCircleFixLeft } from "@/components/utilis/Icons";
 import Footer from "@/components/UI/Footer";
 import { ColorsThemeA } from "@/components/utilis/Themes";
 import CustomInput from "@/components/UI/CustomInput";
+import BaseModal from "@/components/UI/BaseModal";
 
 
 interface AllAvatars {
@@ -34,7 +35,7 @@ const Hairdressers = () => {
     type: "",
     webkitRelativePath: "",
   };
-  
+
   const defaultHairDresser = {
     id: 0,
     hair_salon_id: 0,
@@ -52,7 +53,7 @@ const Hairdressers = () => {
   };
   const hiddenFileInput = React.useRef<any>(null);
   const [hairDressers, setHairDressers] = useState<Hairdresser[]>([]);
-  const [hairDresser, setHairDresser] = useState<Hairdresser>({...defaultHairDresser});
+  const [hairDresser, setHairDresser] = useState<Hairdresser>({ ...defaultHairDresser });
   const [avatarIndex, setAvatarIndex] = useState(1);
   const [showAvatar, setShowAvatars] = useState("men");
   const [avatars, setAvatars] = useState<AllAvatars>(defaultAvatars);
@@ -90,12 +91,12 @@ const Hairdressers = () => {
     let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function () {
-        cb(reader.result)
+      cb(reader.result)
     };
     reader.onerror = function (error) {
-        console.log('Error: ', error);
+      console.log('Error: ', error);
     };
-}
+  }
   const handleProfileImageUpload = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -252,7 +253,7 @@ const Hairdressers = () => {
         .updateHairDresser(hairDresser.id, data)
         .then((res) => {
           getAllHairDresser();
-          setHairDresser({...defaultHairDresser});
+          setHairDresser({ ...defaultHairDresser });
           setProfileImage(() => "");
           setProfileImageBinary(() => "");
           setIsEdit(false);
@@ -318,7 +319,7 @@ const Hairdressers = () => {
     await dashboard.deleteHairDresser(hairDresser.id).then((res) => {
       getAllHairDresser();
       showSnackbar("success", res.data.message);
-      setHairDresser({...defaultHairDresser});
+      setHairDresser({ ...defaultHairDresser });
       setProfileImage(() => "");
       setProfileImageBinary(() => "");
       setAvatarIndex(1);
@@ -333,7 +334,7 @@ const Hairdressers = () => {
   };
 
   const onClear = () => {
-    setHairDresser({...defaultHairDresser});
+    setHairDresser({ ...defaultHairDresser });
     setProfileImage(() => "");
     setProfileImageBinary(() => "");
     setAvatarIndex(1);
@@ -348,7 +349,7 @@ const Hairdressers = () => {
     if (id === avatarIndex) {
       return (
         <div className="relative shadow-[0px_6px_11px_0px_rgba(176,176,176,0.25)] rounded-xl bg-white w-32 h-32">
-          <Image src={image} fill={true} alt="Profile Image" />          
+          <Image src={image} fill={true} alt="Profile Image" />
         </div>
       );
     }
@@ -433,17 +434,71 @@ const Hairdressers = () => {
       });
     }
   }
-
+  // Modal for password
+  const [isModalPswrd, setIsModalPswrd] = useState(false);
 
 
   return (
     <>
+      {isModalPswrd && (
+        <BaseModal close={() => setIsModalPswrd(false)}>
+          <div className="flex flex-col items-center mt-4 mx-4">
+            <div className="w-full mb-8">
+              <CustomInput
+                id="old-password"
+                label="Ancien mot de passe"
+                value={passwordField.old}
+                onChange={(e) => setOldPassword(e.target.value)}
+                type="password"
+              />
+            </div>
+            <div className="w-full mb-8">
+              <CustomInput
+                id="new-password"
+                label="Nouveau mot de passe"
+                value={passwordField.new}
+                onChange={(e) => setNewPassword(e.target.value)}
+                type="password"
+              />
+            </div>
+            <div className="w-full mb-8">
+              <CustomInput
+                id="confirm-new-password"
+                label="Confirmer le nouveau mot de passe"
+                value={passwordField.new2}
+                onChange={(e) => setNew2Password(e.target.value)}
+                type="password"
+              />
+            </div>
+
+            <div className="flex justify-between w-full">
+              <button
+                className={`${Theme_A.button.medWhiteColoredButton} w-1/2 mr-2`}
+                onClick={() => setIsModalPswrd(false)}
+              >
+                Annuler
+              </button>
+
+              <button
+                className={`${Theme_A.button.medBlackColoredButton} w-1/2 ml-2`}
+                onClick={onSubmitPassword}
+              >
+                Confirmer
+              </button>
+            </div>
+          </div>
+        </BaseModal>
+      )}
+
+
+
+
       {isLoading && loadingView()}
       <div className="w-full flex flex-col lg:flex-row items-center justify-center gap-4 mt-8">
         <div className="bg-gradient-to-l  md:block fixed -left-32 md:-left-8 -bottom-32 md:-bottom-8 z-0 mix-blend-overlay ">
           <LogoCircleFixLeft />
         </div>
-        <div className="h-[940px] w-full xl:w-2/5 overflow-auto flex flex-col items-center gap-8 bg-lightGrey rounded-3xl p-4 md:px-12 md:pt-12 md:pb-0 opacity-95">
+        <div className="h-full w-full xl:w-2/5 overflow-auto flex flex-col items-center gap-8 bg-lightGrey rounded-3xl p-4 md:px-12 md:pt-12 md:pb-0 opacity-90 shadow-sm shadow-stone-300">
           <div className={`${Theme_A.textFont.headerH2} underline`}>
             Ajouter un nouveau coiffeur
           </div>
@@ -473,15 +528,23 @@ const Hairdressers = () => {
 
 
           <div className="w-full max-w-[450px]">
+            <div className="flex justify-between items-center gap-4">
+              <CustomInput
+                id="password"
+                label="Mot de passe"
+                value={hairDresser.password}
+                onChange={(e) => onChangePassword(e.target.value)}
+                error={error.password}
+                type="password"
+              />
 
-            <CustomInput
-              id="password"
-              label="Mot de passe"
-              value={hairDresser.password}
-              onChange={(e) => onChangePassword(e.target.value)}
-              error={error.password}
-              type="password"
-            />
+              <button
+                className={`${Theme_A.button.medBlackColoredButton}`}
+                onClick={() => setIsModalPswrd(true)}// Show the modal to modify password
+              >
+                Modifier
+              </button>
+            </div>
           </div>
 
 
@@ -549,7 +612,7 @@ const Hairdressers = () => {
             </div>
             {getAvatars().map((avatar, index) => {
               return (
-                <Avatars key={index} image={avatar.image.includes("http") ? avatar.image : "https://api.onehaircut.com"+avatar.image} id={avatar.id} />
+                <Avatars key={index} image={avatar.image.includes("http") ? avatar.image : "https://api.onehaircut.com" + avatar.image} id={avatar.id} />
               );
             })}
             <div className="cursor-pointer" onClick={handleAvatarNext}>
@@ -613,7 +676,7 @@ const Hairdressers = () => {
         {/* SECTION DROITE */}
 
         {/* COIFFEUR DISPONIBLE */}
-        <div className="h-[940px] w-full xl:w-2/5 overflow-auto flex flex-col items-center justify-start gap-8 bg-lightGrey rounded-3xl p-4 md:p-12">
+        <div className="h-auto w-full xl:w-2/5 overflow-auto flex flex-col items-center justify-start gap-8 bg-lightGrey rounded-3xl p-4 md:p-12 shadow-sm shadow-stone-300">
           <div className={`${Theme_A.textFont.headerH2} underline`}>
             Coiffeur(s)/-euse(s) disponible(s)
           </div>
@@ -630,7 +693,7 @@ const Hairdressers = () => {
                       <Image
                         fill={true}
                         src={
-                          item.profile_image ? (item.profile_image.includes("http") ? item.profile_image : 'https://api.onehaircut.com'+item.profile_image)  : (item.avatar && item.avatar.image) ? `https://api.onehaircut.com${item.avatar.image}` : ''
+                          item.profile_image ? (item.profile_image.includes("http") ? item.profile_image : 'https://api.onehaircut.com' + item.profile_image) : (item.avatar && item.avatar.image) ? `https://api.onehaircut.com${item.avatar.image}` : ''
                         }
                         alt="image"
                       />
