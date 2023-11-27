d22d9b08d2720e02857c11125f496e21863b6fc6
silverknight59222 <silverknight59222@gmail.com>
2023-11-27 22:14:46 +0100

    Responsive improvement on reservation pages

--- src/components/pages/SearchSalon/index.tsx
+++ src/components/pages/SearchSalon/index.tsx
@@ -13,7 +13,7 @@ import userLoader from "@/hooks/useLoader";
 import { getLocalStorage, setLocalStorage } from "@/api/storage";
 import { Theme_A } from "@/components/utilis/Themes";
 import ChatModal from "./ChatModal";
-import { GoogleMap, Marker, useJsApiLoader, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, Marker, useJsApiLoader, LoadScript, MarkerF } from '@react-google-maps/api';
 import Footer from "@/components/UI/Footer";
 import SalonPicModal from "./SalonPicModal";
 import PerfSampleModal from "./PerfSampleModal";
@@ -21,6 +21,7 @@ import MapIcon from "@/components/utilis/Icons";
 import ReactDOMServer from 'react-dom/server';
 import { dashboard } from '@/api/dashboard';
 import { elements } from "chart.js";
+import { Address } from "@/types"
 
 
 const temp = getLocalStorage("haircut")
@@ -36,6 +37,7 @@ interface SalonHairdressers {
   name: string
 }
 interface SalonProfile {
+  address: Address;
   name: string,
   rating: number,
   description: string,
@@ -140,12 +142,6 @@ const SearchSalon = () => {
 
 
   //GOOGLE MAP 
-  //TODO Centrer par rapport aux coordonnées du salon
-  const mapCenter = {
-    lat: 47.146263032510866, // Latitude du centre de votre carte
-    lng: 7.27569477025937  // Longitude du centre de votre carte
-  };
-
   const mapStyles = {
     height: "500px",
     width: "100%",
@@ -163,13 +159,13 @@ const SearchSalon = () => {
   const mapIconUrl = `data:image/svg+xml;base64,${btoa(mapIconSvg)}`;
 
 
-  
+
 
 
   return (
     <div className="relative">
       {/* Affiche une vue de chargement pendant que les données sont récupérées */}
-   
+
       {isLoading && loadingView()}
 
       {/* Barre de navigation */}
@@ -212,10 +208,37 @@ const SearchSalon = () => {
         {/* Section d'image et d'informations du salon */}
         <div className="flex flex-col items-center md:items-center xl:justify-between gap-11">
 
-          <div className="w-full flex flex-col md:flex-row gap-8 2xl:gap-12 bg-gray-50 p-2 rounded-2xl">
+          {/* Nom et notation du salon */}
+          <div className="flex flex-col items-start md:items-center md:justify-center flex-grow">
+            {salonProfile && <p className="text-black font-bold text-3xl xl:text-4xl 2xl:text-5xl">
+              {salonProfile.name}
+            </p>}
+            {/*TODO import real rating on the selected haircut {salonProfile.rating} */}
+            {salonProfile && <div className="flex items-center gap-1 border-b-2 border-[#DBDBDB] text-xl 2xl:text-2xl font-semibold text-black pb-3 mt-1">
+              <StarRatings
+                rating={salonProfile.haircut ? salonProfile.haircut.rating : salonProfile.rating}
+                starRatedColor="#FEDF10"
+                starSpacing="4px"
+                starDimension="25px"
+                numberOfStars={5}
+                name="rating"
+              />
+              {/* TODO use salon's rating of the selected haircut {salonProfile.rating}*/}
+              <p className="-mb-2"> {salonProfile.haircut ? salonProfile.haircut.rating : salonProfile.rating}</p> <br /> <small><small>  ({salonProfile.haircut ? salonProfile.haircut.rating_counts : salonProfile.rating_counts} avis</small></small> <p className="font-normal"><small><small><small>* sur cette coiffure</small></small></small> <br /></p> <small><small> ) </small></small>
+            </div>}
+            {/* Description du salon */}
+            <div className="mt-5 p-4 bg-gray-200 w-full lg:w-[400px] 2xl:w-[720px] rounded-xl ">
+              {salonProfile && <p className="text-black text-lg">
+                {/* TODO lien vers la description du salon {salonProfile.description} */}
+                {salonProfile.description}
+              </p>}
+            </div>
+          </div>
+
+          <div className="w-full flex flex-col sm:flex-row gap-4 2xl:gap-12 bg-gray-50 p-2 rounded-2xl justify-evenly">
             <div className="flex flex-col items-start">
               {/* Conteneur principal de l'image */}
-              <div className="w-[320px] lg:w-[500px] 2xl:w-[600px] h-64 lg:h-[500px] relative rounded-4xl p-2 mb-3 ">
+              <div className="w-full sm:w-[350px] md:w-[400px] lg:w-[500px] 2xl:w-[600px] h-64 sm:h-80 md:h-60 lg:h-[500px] relative rounded-4xl p-2 mb-3 ">
 
                 {/* Image principale */}
                 {salonProfile && <div className="w-full h-full relative rounded-4xl ">
@@ -230,7 +253,7 @@ const SearchSalon = () => {
               </div>
 
               {/* Conteneur pour les miniatures et les titres */}
-              <div className="flex justify-between mt-3 w-[320px] lg:w-[500px] 2xl:w-[600px] relative">
+              <div className="flex justify-between mt-3 w-full lg:w-[500px] 2xl:w-[600px] relative">
 
                 {/* Conteneur gauche (image + titre) */}
                 <div className="flex flex-col items-center ">
@@ -241,7 +264,7 @@ const SearchSalon = () => {
                   >
                     {/* TODO charger les images vitrines ici */}
                     {salonProfile && salonProfile.salon_images && <Image
-                      src={salonProfile.salon_images.filter((image)=>image.type == "showcase")[0]?.image.includes('http') ? salonProfile.salon_images.filter((image)=>image.type == "showcase")[0]?.image : `https://api.onehaircut.com${salonProfile.salon_images.filter((image)=>image.type == "showcase")[0]?.image}`}
+                      src={salonProfile.salon_images.filter((image) => image.type == "showcase")[0]?.image.includes('http') ? salonProfile.salon_images.filter((image) => image.type == "showcase")[0]?.image : `https://api.onehaircut.com${salonProfile.salon_images.filter((image) => image.type == "showcase")[0]?.image}`}
                       alt="Image miniature gauche"
                       layout="fill"
                       objectFit="cover"
@@ -262,7 +285,7 @@ const SearchSalon = () => {
                   >
                     {/* TODO charger les images vitrines ici */}
                     {salonProfile && salonProfile.salon_images && <Image
-                      src={salonProfile.salon_images.filter((image)=>image.type == "hairstyle")[0]?.image.includes('http') ? salonProfile.salon_images.filter((image)=>image.type == "hairstyle")[0]?.image : `https://api.onehaircut.com${salonProfile.salon_images.filter((image)=>image.type == "hairstyle")[0]?.image}`}
+                      src={salonProfile.salon_images.filter((image) => image.type == "hairstyle")[0]?.image.includes('http') ? salonProfile.salon_images.filter((image) => image.type == "hairstyle")[0]?.image : `https://api.onehaircut.com${salonProfile.salon_images.filter((image) => image.type == "hairstyle")[0]?.image}`}
                       alt="Image miniature gauche"
                       layout="fill"
                       objectFit="cover"
@@ -276,36 +299,11 @@ const SearchSalon = () => {
             </div>
 
 
-            {/* Nom et notation du salon */}
-            <div className="flex flex-col items-start md:items-center md:justify-center flex-grow">
-              {salonProfile && <p className="text-black font-bold text-3xl xl:text-4xl 2xl:text-5xl">
-                {salonProfile.name}
-              </p>}
-              {/*TODO import real rating on the selected haircut {salonProfile.rating} */}
-              {salonProfile && <div className="flex items-center gap-1 border-b-2 border-[#DBDBDB] text-xl 2xl:text-2xl font-semibold text-black pb-3 mt-1">
-                <StarRatings
-                  rating={salonProfile.haircut ? salonProfile.haircut.rating : salonProfile.rating}
-                  starRatedColor="#FEDF10"
-                  starSpacing="4px"
-                  starDimension="25px"
-                  numberOfStars={5}
-                  name="rating"
-                />
-                {/* TODO use salon's rating of the selected haircut {salonProfile.rating}*/}
-                <p className="-mb-2"> {salonProfile.haircut ? salonProfile.haircut.rating : salonProfile.rating }</p> <br /> <small><small>  ({salonProfile.haircut ? salonProfile.haircut.rating_counts : salonProfile.rating_counts} avis</small></small> <p className="font-normal"><small><small><small>* sur cette coiffure</small></small></small> <br /></p> <small><small> ) </small></small>
-              </div>}
-              {/* Description du salon */}
-              <div className="mt-5 p-4 bg-gray-200 w-full lg:w-[400px] 2xl:w-[720px] rounded-xl ">
-                {salonProfile && <p className="text-black text-lg">
-                  {/* TODO lien vers la description du salon {salonProfile.description} */}
-                  {salonProfile.description}
-                </p>}
-              </div>
-            </div>
+
 
             {/* Horaires et bouton de réservation */}
             <div className="w-full md:w-auto flex flex-col items-center justify-center">
-              <div className="flex flex-col gap-6 bg-white  opacity-90 w-full sm:w-[300px] md:w-[350px] xl:w-[420px] 2xl:w-[470px] border border-[#E1E1E1] rounded-3xl py-6 px-8 2xl:px-10 shadow-sm shadow-stone-600">
+              <div className="flex flex-col gap-6 bg-white  opacity-90 w-full sm:w-[270px] md:w-[300px] xl:w-[420px] 2xl:w-[470px] border border-[#E1E1E1] rounded-3xl py-6 px-8 2xl:px-10 shadow-sm shadow-stone-600">
                 {/* Titre ajouté ici */}
                 <h2 className="text-xl 2xl:text-2xl font-semibold text-[#272727] mb-4 text-center">
                   Horaire d'ouverture
@@ -348,11 +346,11 @@ const SearchSalon = () => {
             </p>
 
             {/* Conteneur pour les informations */}
-            <div className="flex flex-col items-center gap-6 bg-white opacity-90 w-full max-w-[90vw] sm:max-w-[300px] md:max-w-[350px] xl:max-w-[420px] 2xl:max-w-[500px] border border-[#E1E1E1] rounded-3xl py-6 px-8 2xl:px-10 shadow-sm shadow-stone-600 mt-6 ">
+            <div className="flex flex-col items-center gap-6 bg-white opacity-90 w-[420px] xl:max-w-[420px] 2xl:max-w-[500px] border border-[#E1E1E1] rounded-3xl py-6 px-8 2xl:px-10 shadow-sm shadow-stone-600 mt-6 ">
 
               {/* Ligne d'information avec titre et valeur */}
               {/* Ligne avec Prix et Durée totale */}
-              <div className="flex justify-between w-full text-black ">
+              <div className="flex justify-between w-full text-black gap-2 ">
                 {/* Partie gauche : Prix total */}
                 <div className="flex justify-evenly w-1/2">
                   <p className="text-md xl:text-lg font-semibold ">
@@ -463,14 +461,13 @@ const SearchSalon = () => {
           </div>
         </div>
 
-        {/*TODO import real adress of the salon */}
+        {/*TODO import real address of the salon */}
         <div className="mt-20 bg-stone-50 p-8 rounded-lg shadow-sm  opacity-90">
           <p className="text-3xl xl:text-4xl font-semibold text-black text-center">
             Adresse du salon
           </p>
-          {salonProfile != null && <p className="mt-6 text-lg xl:text-xl font-medium text-black text-center">
-            {/*Import Salon's adress here  */}
-            {salonProfile.name}
+          {salonProfile != null && <p className="mt-6 text-lg xl:text-xl font-normal text-black text-center">
+            {salonProfile.address.city},  {salonProfile.address.country}
           </p>}
         </div>
         {/* Import Google map here */}
@@ -478,12 +475,12 @@ const SearchSalon = () => {
           <GoogleMap
             mapContainerStyle={mapStyles}
             zoom={13}
-            center={mapCenter}
+            center={{ lat: salonProfile?.address.lat!, lng: salonProfile?.address.long! }}
           >
             {/* Ajout d'un marqueur */}
             {/* TODO Position du salon ici */}
-            <Marker
-              position={mapCenter}
+            <MarkerF
+              position={{ lat: salonProfile?.address.lat!, lng: salonProfile?.address.long! }}
               icon={mapIconUrl}
             />
 
