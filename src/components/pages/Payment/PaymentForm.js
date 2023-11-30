b42da5cb74695b93b45e8978e151c23829c83415
silverknight59222 <silverknight59222@gmail.com>
2023-11-30 18:00:21 +0530

    resolving issues

--- src/components/pages/Payment/PaymentForm.js
+++ src/components/pages/Payment/PaymentForm.js
@@ -3,11 +3,20 @@ import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 import axios from 'axios';
 import { client } from "@/api/clientSide";
 import { Theme_A } from "@/components/utilis/Themes";
+import { getLocalStorage } from '@/api/storage';
 
 const PaymentForm = ({onSuccess}) => {
   const stripe = useStripe();
   const elements = useElements();
   const [loading, setLoading] = useState(false);
+  const salon = getLocalStorage('selectedSalon')
+  const user = getLocalStorage('user');
+  const userData = user ? JSON.parse(user) : null
+  const salonData = salon ? JSON.parse(salon) : null
+  const amount = salonData ? salonData.final_price * 100 : 0
+
+  const customerEmail = userData ? userData.email : ''
+  const serviceDescription = salonData ? 'Payment for Services at ' + salonData.name : 'Payment for Services';  
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -18,21 +27,31 @@ const PaymentForm = ({onSuccess}) => {
 
     setLoading(true);
 
-    const { token, error } = await stripe.createToken(elements.getElement(CardElement));
+    const { token, error } = await stripe.createToken(elements.getElement(CardElement),{
+      email: customerEmail
+    });
 
     if (error) {
       console.error(error);
       setLoading(false);
       return;
     }    
-
-    const response = await client.paymentIntent({amount: 1000, token: token.id});    
+    
+    const response = await client.paymentIntent(
+      {
+        amount: amount,
+        token: token.id,
+        email: customerEmail,
+        description: serviceDescription
+      }
+    );    
     const clientSecret = response.data.clientSecret;
 
     const result = await stripe.confirmCardPayment(clientSecret, {
       payment_method: {
         card: elements.getElement(CardElement),
       },
+      receipt_email: customerEmail, // Optional: Include customer email in the payment request            
     });
 
     if (result.error) {
