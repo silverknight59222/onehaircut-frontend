d415209c78621401aa2f2be7d7eb4fde13c83868
silverknight59222 <silverknight59222@gmail.com>
2023-12-06 22:39:46 +0100

    Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,7 @@ export default function RootLayout({
 						theme="light"
 					/>
 					{children}
+					<Analytics />
 				</body>
 			</RouteGuard>
 		</html>
