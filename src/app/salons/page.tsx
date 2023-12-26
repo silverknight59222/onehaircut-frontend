093c7d6f150261492abf0590412ec356d0ce2ba8
silverknight59222 <silverknight59222@gmail.com>
2023-12-27 04:34:38 +0700

    Can Merge - Fix window is not defined

--- src/app/salons/page.tsx
+++ src/app/salons/page.tsx
@@ -1,12 +1,20 @@
+// import ChooseSalon from '@/components/pages/ChooseSalon'
+// import React from 'react'
+// const page = () => {
+//     return (
+//       <div><ChooseSalon/></div>
+//     )
+// }
+
+// export default page
+
 import ChooseSalon from '@/components/pages/ChooseSalon'
 import React from 'react'
 
-const page = () => {
-  if(typeof window !== "undefined"){
-    return (
-      <div><ChooseSalon/></div>
-    )
-  }
-}
+const isSSREnabled = () => typeof window === 'undefined';
 
-export default page
+export default function Home() {
+  return (
+    !isSSREnabled() && <ChooseSalon />
+  );
+}
--- src/components/pages/ChooseSalon/index.tsx
+++ src/components/pages/ChooseSalon/index.tsx
@@ -78,13 +78,11 @@ const SalonChoice = () => {
         const updateDimension = () => {
             setScreenSize(getCurrentDimension())
         }
-        if(typeof window !== 'undefined'){
-            window.addEventListener('resize', updateDimension);
-            return(() => {
-                window.removeEventListener('resize', updateDimension);
-            })
-        }
+        window.addEventListener('resize', updateDimension);
     
+        return(() => {
+            window.removeEventListener('resize', updateDimension);
+        })
     },[screenSize])
 
 
@@ -153,22 +151,20 @@ const SalonChoice = () => {
             const salonInRating = (ratingFilter.length === 0) || ((ratingFilter.length > 0) && (ratingFilter.includes(salon.rating))) || (newSalonFilter && salon.rating === 0);
             const salonInCountry = (countryFilter === '') || (salon.address.country === countryFilter);
             const frenchToEnglishMapping = {
-                'Lundi' : 1,
-                'Mardi' : 2,
-                'Mercredi' : 3,
-                'Jeudi' : 4,
-                'Vendredi' : 5,
-                'Samedi' : 6,
-                'Dimanche' : 0
+                'Lundi': 1,
+                'Mardi': 2,
+                'Mercredi': 3,
+                'Jeudi': 4,
+                'Vendredi': 5,
+                'Samedi': 6,
+                'Dimanche': 0
             };
             let salonAvailable = true;
-            
 
-            for(const day of availabilityFilter)
-            {
+
+            for (const day of availabilityFilter) {
                 salonAvailable = salon.openTimes[frenchToEnglishMapping[day]].available;
-                if(salonAvailable)
-                {
+                if (salonAvailable) {
                     break;
                 }
             }
@@ -203,7 +199,7 @@ const SalonChoice = () => {
                 positionArray.push({ lat: Number(fsalon.address.lat), lng: Number(fsalon.address.long) })
             }
         })
-        
+
         console.log('position array', positionArray)
         
         // recalculateMap(positionArray)
