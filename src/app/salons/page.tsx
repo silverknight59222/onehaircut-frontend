c7ad1ba8663dd9c4253227610b34873854d0f600
silverknight59222 <silverknight59222@gmail.com>
2023-12-26 22:42:31 +0700

    Merge OK - fix window is not defined

--- src/app/salons/page.tsx
+++ src/app/salons/page.tsx
@@ -2,7 +2,7 @@ import ChooseSalon from '@/components/pages/ChooseSalon'
 import React from 'react'
 
 const page = () => {
-  if(typeof window !== 'undefined'){
+  if(typeof window !== "undefined"){
     return (
       <div><ChooseSalon/></div>
     )
