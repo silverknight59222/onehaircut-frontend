630c5862d059ab45de43d8f3b432e4c4563fb507
silverknight59222 <silverknight59222@gmail.com>
2024-01-15 02:53:24 +0700

    Cherry pick - update salon avatar bug

--- src/components/pages/welcome/index.tsx
+++ src/components/pages/welcome/index.tsx
@@ -342,7 +342,7 @@ const Welcome = () => {
         }
         else {
           setPreviewImage('')
-          if (resp.data.status == 200) {
+          if(resp.data.status == 200){
             showSnackbar('success', resp.data.message)
           }
           else {
@@ -432,7 +432,7 @@ const Welcome = () => {
     }
     console.log(hasPreview)
     hasPreview.filter((preview_image) => {
-      if (preview_image === String(selectedHaircut.id)) {
+      if(preview_image === String(selectedHaircut.id)){
         console.log("ID PREVIEW : " + preview_image);
         console.log("ID SELECTED : " + selectedHaircut.id);
       }
@@ -521,9 +521,8 @@ const Welcome = () => {
               Démonstration d’utilisation
             </div>
             <div onClick={() => router.push('/login')}
-              id="Connexion - Inscription"
               className={`p-2 sm:p-4 md:p-5 text-center cursor-pointer ${LogInButton}`}>
-              Connexion - Inscription
+              Connexion / Inscription
             </div>
             {/*
             <div className="p-2 sm:p-4 md:p-5 text-center border-[#FE3462] border-2 rounded-2xl cursor-pointer mr-3">
