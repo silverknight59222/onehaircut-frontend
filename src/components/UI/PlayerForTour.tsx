090f9c321448890680dc2839537155488a33bb74
silverknight59222 <silverknight59222@gmail.com>
2024-02-04 08:36:29 +0100

    CAN MERGE - Player icon is reset while the audio is ended

--- src/components/UI/PlayerForTour.tsx
+++ src/components/UI/PlayerForTour.tsx
@@ -5,10 +5,14 @@ interface AudioPlayerProps {
   src: string;
 }
 
+// This module is meant to be used to play audio during the tour.
+// it uses therefore special icons to to easily detected by the user
+// a click on this icon plays the audio
 const AudioPlayerForTour: React.FC<AudioPlayerProps> = ({ src }) => {
   const audioRef = useRef<HTMLAudioElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
+  // handle the click on the play pause icon
   const togglePlayPause = () => {
     const audio = audioRef.current;
     if (audio) {
@@ -21,11 +25,16 @@ const AudioPlayerForTour: React.FC<AudioPlayerProps> = ({ src }) => {
     }
   };
 
+  // reset icon to start, when the music is done
+  const handleAudioEnd = () => {
+    setIsPlaying(false);
+  };
+
   return (
     <div className={"m-1"}>
       {!isPlaying && <FaDeaf onClick={togglePlayPause} size={20} color={""} />}
       {isPlaying && <FaRegPauseCircle onClick={togglePlayPause} size={20} color={""} />}
-      <audio ref={audioRef}>
+      <audio ref={audioRef} onEnded={handleAudioEnd}>
         <source src={src} type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
